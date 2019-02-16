a878d94b8f496b1b19d806c2be5d1dc7e7f73562
Zdenek Grůza <aladin@atlayo.com>
2019-02-16 18:13:08 +0100

    Update put_onion.php

--- put_onion.php
+++ put_onion.php
@@ -15,8 +15,23 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$setup = "CREATE TABLE `onions` (
+  `id` int(11) NOT NULL,
+  `onion` text COLLATE utf8_unicode_ci NOT NULL DEFAULT 0,
+  `description` text COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+
 $sql = "INSERT INTO onions (onion, description)
 VALUES ('$onion', '$des')";
+
+if(isset($_GET['setup']) && isset($_GET['setup'])){
+	if ($conn->query($setup) === TRUE) {
+	   echo "Setup database is successfully installed!<br />Now run C# Code. :) ";
+	} else {
+	    echo "Database Connection Error! Change credetains and try it again.";
+
+	}
+}
 
 if ($conn->query($sql) === TRUE) {
    echo "New record created successfully";
